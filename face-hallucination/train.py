33d8a8778804370ae5871241f3ac541e297bbb37
Prithvi Raju <nprithviraj24@gmail.com>
2020-02-04 21:03:20 +0700

    Face hallucination added

---
+++ face-hallucination/train.py
@@ -0,0 +1,8 @@
+#importing Generator AND Discriminator for high-to-low
+from High-to-Low import Generator as G_H2L  
+from High-to-Low import Discriminator as D_H2L
+
+
+#Similarly importing Generator AND Discriminator for high-to-low
+from Low-to-High import Generator as G_L2H  
+from Low-to-High import Discriminator as D_L2H
