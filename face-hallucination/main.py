1f8b7db1a80bbbdc7f91d3268066d79c8385a15e
Prithvi <nprithviraj24@gmail.com>
2020-02-16 19:03:40 +0700

    .

--- face-hallucination/main.py
+++ face-hallucination/main.py
@@ -8,12 +8,12 @@ import os
 import utilities as utility
 
 #importing Generator AND Discriminator for high-to-low
-from High2Low import Generator as G_H2L  
-from High2Low import Discriminator as D_H2L
+from High2Low import generator as G_H2L  
+from High2Low import discriminator as D_H2L
 
 #Similarly importing Generator AND Discriminator for low to high l2h
 from Low2High import GEN_DEEP as G_L2H  
-from Low2High import Discriminator as D_L2H
+from Low2High import discriminator as D_L2H
 
 #import dataloader
 import dataloader
